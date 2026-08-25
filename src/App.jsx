2ba3afd1a7f3687ffa60126284b78744005a8226
Devidas tambe <devitambe102@gmail.com>
2026-08-25 16:04:57 +0530

    react folder structutr

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>hello world</h1>
+    <h1 class="heading"> Welcome to reactjs</h1>
+    <p>good afternoon</p>
+    </>
+  )
+}
+
+export default App

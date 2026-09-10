95605ef481689b4882fb31542e3008db8c62009c
Devidas Tambe <devitambe102@gmail.com>
2026-09-10 19:03:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 function App() { 
   const [count, setCount] = useState(0)  
 
-  return (
+  return ( 
     <> 
     <h1>Hello World</h1>
     <h1 class="heading"> Welcome to reactjs</h1>
